24a880dd9feb90658ad6613ed950a3eeab805b8b
ampeo <ampeo@yandex.ru>
2020-02-14 15:43:49 +0300

    2 task

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 str = input('Введите текст: ')
+str.lower()
 
 list = str.split(' ')
 #Разбиваем текст на слова
 dictionary = {}
-
 
 for index in range(0, len(list)):
 	str = list[index]
@@ -24,4 +24,14 @@ for index in range(0, len(list)):
 	else:
 		dictionary[str] = dictionary[str] + 1
 
-	print(dictionary)
+#2 Задание
+topWords = sorted(dictionary.items(), key = lambda count: count[1])
+#Сортируем элементы по значению
+#Нельзя перевернуть с помощью reverse(), т к кортеж - неизменяемый список
+
+str = ''
+#отделяем клю(слово) от кортежа и закидываем с строку
+for index in range(len(topWords) - 1, len(topWords) - 11, -1):
+	word = topWords[index]
+	str = str + ' ' + word[0]
+print(str + '.')
